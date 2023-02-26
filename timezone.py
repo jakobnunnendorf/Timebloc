5315eea1450203b052ca67a598d30485104b86bc
“Jakob <jnunnendorf@icloud.com>
2023-02-26 20:28:16 +0200

    added utc format for current timezone

--- timezone.py
+++ timezone.py
@@ -7,4 +7,10 @@ location = json.loads(response.content)
 def return_current_timezone():
     return location['timezone']
 
+import datetime, pytz
+
+def return_current_timezone_utc():
+    return datetime.datetime.now(pytz.timezone(return_current_timezone())).strftime('%z')
+
 # print(return_current_timezone())
+# print(return_current_timezone_utc())
